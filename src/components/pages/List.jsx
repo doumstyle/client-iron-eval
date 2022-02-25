68245c153692c350706667e5d8bbbb3a51148fc1
Mamadou Ngatté <doumstyle@gmail.com>
2022-02-25 03:13:09 +0100

    fetch heroes from API and display their name and intelligence level in the List view

--- src/components/pages/List.jsx
+++ src/components/pages/List.jsx
@@ -1,18 +1,39 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 // import axios from 'axios';
 // import apiHandler from './../../api/handler';
 
 const List = () => {
-	const [list, setList] = useState([]);
+  const [list, setList] = useState([]);
 
-	useEffect(() => {}, []);
+  useEffect(() => {
+    axios
+      .get("https://backend-jury.herokuapp.com/api/heroes")
+      .then((response) => {
+        console.log("Response >>>", response.data);
+        setList(response.data);
+      })
+      .catch((err) => {
+        console.error(error);
+      });
+  }, []);
 
-	return (
-		<>
-			<h1>Let's list, shall we ?</h1>
-			<p>This component should display resources you must fetch.</p>
-		</>
-	);
+  return (
+    <>
+      <h1>Let's list, shall we ?</h1>
+      <p>This component should display resources you must fetch.</p>
+      <ul>
+        {list &&
+          list.map((hero) => {
+            return (
+              <li key={hero._id}>
+                {hero.name} - {hero.powerstats.intelligence} smarts
+              </li>
+            );
+          })}
+      </ul>
+    </>
+  );
 };
 
 export default List;
